792ee64d20871fcd5f553fc68b80da05f46ac886
Daeho Ro <lamanus@outlook.kr>
2025-03-27 18:06:35 +0900

    day-05: ui

--- section-02/src/pages/search/index.tsx
+++ section-02/src/pages/search/index.tsx
@@ -1,11 +1,15 @@
-import { useRouter } from 'next/router';
 import SearchableLayout from '@/components/searchable-layout';
+import BookItem from '@/components/book-item';
+import books from '@/mock/books.json';
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: React.ReactNode) => {
